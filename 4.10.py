fa1491dd020de50bc30d05f7aa4e326f725e3de4
Pranchal <127619615+PranchalSehgal@users.noreply.github.com>
2024-03-31 16:22:36 +0530

    Add files via upload

---
+++ 4.10.py
@@ -0,0 +1,11 @@
+def main():
+    numbers = [15, 13, 11, 9, 7]
+    product = 1
+
+    for num in numbers:
+        product *= num
+
+    print("Product of the series:", product)
+
+if __name__ == "__main__":
+    main()
---
+++ 4.2.py
@@ -0,0 +1,12 @@
+def main():
+    num = 1
+    max_num = 5
+
+    print("Printing numbers using a while loop:")
+
+    while num <= max_num:
+        print(num)
+        num += 1
+
+if __name__ == "__main__":
+    main()
